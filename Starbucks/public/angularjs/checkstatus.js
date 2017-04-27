60d52883be8391470cd6617413406c43902073ab
amrutadh <amrutadh@gmail.com>
2017-04-27 00:11:04 -0700

    cleaned up

--- Starbucks/public/angularjs/checkstatus.js
+++
@@ -1,6 +0,0 @@
-var StarbucksApp = angular.module('StarbucksApp', ['ngRoute']);
-
-StarbucksApp.controller('checkstatusController',['$scope', function($scope) {
-   console.log("check status controller");
-
-}]);
